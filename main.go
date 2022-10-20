188c2cd45dd9c4cc6facc5e7009499f1add6b02b
hsmtkk <hsmtkk@gmail.com>
2022-10-20 13:46:48 +0900

    get port from env var

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
+	port, err := getPort()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	// Echo instance
 	e := echo.New()
 
@@ -19,10 +28,30 @@ func main() {
 	e.GET("/", hello)
 
 	// Start server
-	e.Logger.Fatal(e.Start(":1323"))
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", port)))
 }
 
 // Handler
 func hello(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, World!")
+}
+
+func requiredEnv(key string) (string, error) {
+	val := os.Getenv(key)
+	if val == "" {
+		return "", fmt.Errorf("you must define %s environment variable", key)
+	}
+	return val, nil
+}
+
+func getPort() (int, error) {
+	portStr, err := requiredEnv("PORT")
+	if err != nil {
+		return 0, err
+	}
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+		return 0, fmt.Errorf("failed to parse %s as int; %w", portStr, err)
+	}
+	return port, nil
 }
